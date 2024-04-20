35048707ba71d7f14f5ae82bf046a1b25dae422b
eric carpo <carpoeric@gmail.com>
2024-04-19 18:13:47 -0700

    Finished project, may try to add some customization and displays

---
+++ Neighborhood-Library/src/main/java/com/pluralsight/Book.java
@@ -0,0 +1,113 @@
+package com.pluralsight;
+
+
+
+class Book
+{
+    // Properties
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
+
+    // Constructor
+    public Book(int id, String isbn, String title)
+    {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = false;
+        this.checkedOutTo = "";
+    }
+
+    // G&S
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getIsbn()
+    {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn)
+    {
+        this.isbn = isbn;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public boolean isCheckedOut()
+    {
+        return isCheckedOut;
+    }
+
+    public String getCheckedOutTo()
+    {
+        return checkedOutTo;
+    }
+
+    // Checkout Methods
+    public void checkOut(String name)
+    {
+        if (!isCheckedOut)
+        {
+            isCheckedOut = true;
+            checkedOutTo = name;
+            System.out.println("Book checked out to " + name);
+
+        }
+
+        else
+        {
+            System.out.println("This book is already checked out.");
+        }
+    }
+
+    // Check In Methods
+    public void checkIn()
+    {
+        if (isCheckedOut)
+        {
+            isCheckedOut = false;
+            checkedOutTo = "";
+            System.out.println("Book checked in successfully.");
+        }
+
+        else
+        {
+            System.out.println("This book is not currently checked out.");
+        }
+    }
+
+    // toString for Book info
+
+    public String toString()
+    {
+        if (isCheckedOut)
+        {
+            return "Book ID: " + id + ", Title: " + title + ", ISBN: " + isbn + ", Checked Out: " + isCheckedOut +
+                    ", Checked Out To: " + checkedOutTo;
+        }
+
+        else
+        {
+            return "Book ID: " + id + ", Title: " + title + ", ISBN: " + isbn + ", Checked Out: " + isCheckedOut;
+        }
+    }
+}
---
+++ Neighborhood-Library/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,170 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Main
+{
+    static Scanner userInput = new Scanner(System.in);
+    static final int MAX_BOOKS = 20;
+    static int bookCount = 0;
+    static final Book[] books = new Book[MAX_BOOKS];
+
+
+    public static void main(String[] args)
+    {
+        // Array
+        preloadBooks();
+
+        int choice = 0;
+        while (choice != 3)
+        {
+            //Home Screen Print
+            System.out.println();
+            System.out.println("Welcome to your Neighborhood Library!");
+            System.out.println("------------------------------");
+            System.out.println("1 - Show Available Books");
+            System.out.println("2 - Show Checked Out Books");
+            System.out.println("3 - Exit (Close App)");
+            System.out.println("------------------------------");
+            System.out.println();
+            System.out.println("To select an action, please enter it's corresponding number here: ");
+            choice = Integer.parseInt(userInput.nextLine());
+
+            switch (choice)
+            {
+                case 1:
+                    showAvailableBooks(books);
+                    break;
+                case 2:
+                    showCheckedOutBooks(books);
+                    break;
+                case 3:
+                    System.out.println();
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println();
+                    System.out.println("Invalid selection");
+                    break;
+            }
+        }
+    }
+
+    public static void preloadBooks()
+{
+    books[0] = new Book(1, "6789998212", "The Lightning Thief");
+    books[1] = new Book(2, "6789998213", "The Sea Of Monsters");
+    books[2] = new Book(3, "6789998214", "The Titan's Curse");
+    books[3] = new Book(4, "6789998215", "The Battle of the Labyrinth");
+    books[4] = new Book(5, "6789998216", "The Last Olympian");
+    books[5] = new Book(6, "6789998217", "The Chalice of the Gods");
+    books[6] = new Book(7, "6789998218", "Twilight");
+    books[7] = new Book(8, "6789998219", "New Moon");
+    books[8] = new Book(9, "6789998220", "Breaking Dawn");
+    books[9] = new Book(10, "6789998221", "Eclipse");
+    books[10] = new Book(11, "6789998222", "The Sorcerer's Stone");
+    books[11] = new Book(12, "6789998223", "The Chamber of Secrets");
+    books[12] = new Book(13, "6789998224", "The Prisoner of Azkaban");
+    books[13] = new Book(14, "6789998225", "The Goblet of Fire");
+    books[14] = new Book(15, "6789998226", "The Order of the Phoenix");
+    books[15] = new Book(16, "6789998227", "The Half-Blood Prince");
+    books[16] = new Book(17, "6789998228", "The Deathly Hallows");
+    books[17] = new Book(18, "6789998229", "The Hunger Games");
+    books[18] = new Book(19, "6789998230", "Catching Fire");
+    books[19] = new Book(20, "6789998231", "Mockingjay");
+    bookCount = 20;
+}
+
+    public static void showAvailableBooks(Book[] books)
+    {
+        System.out.println("Here's all the available books!");
+        for (int i = 0; i < bookCount; i++)
+        {
+            if (!books[i].isCheckedOut())
+            {
+                System.out.println("Book " + (i + 1) + ":");
+                System.out.println(books[i]);
+                System.out.println();
+            }
+        }
+
+
+        System.out.println("Please enter the book number you would like to check out, or enter 0 to go back to the home screen: ");
+        int userChoice = Integer.parseInt(userInput.nextLine());
+
+        if (userChoice == 0)
+        {
+            return;
+        }
+        else if (userChoice >= 1 && userChoice <= bookCount)
+        {
+            System.out.print("Enter your name: ");
+            String userName = userInput.nextLine();
+
+            Book selectedBook = books[userChoice - 1];
+            selectedBook.checkOut(userName);
+            System.out.println();
+            System.out.println("Thank you for checking out \"" + selectedBook.getTitle() + "\"!");
+            System.out.println("Redirecting you back to the home page now...");
+        }
+        else
+        {
+            System.out.println("Invalid selection. Please enter a valid book number.");
+        }
+    }
+
+    public static void showCheckedOutBooks(Book[] books)
+    {
+        System.out.println("Here's all the checked out books:");
+        boolean hasCheckedOutBooks = false;
+        for (int i = 0; i < bookCount; i++)
+        {
+            if (books[i].isCheckedOut())
+            {
+                hasCheckedOutBooks = true;
+                System.out.println("Book " + (i + 1) + ":");
+                System.out.println(books[i]);
+                System.out.println();
+            }
+        }
+
+        if (!hasCheckedOutBooks)
+        {
+            System.out.println("No books are currently checked out.");
+        }
+
+
+        System.out.println("Enter C to check in a book, or enter X to go back to the home screen: ");
+        String userChoice = userInput.nextLine();
+
+        switch (userChoice.toUpperCase())
+        {
+            case "C":
+                checkInBook();
+                break;
+            case "X":
+                return; // Go back home
+            default:
+                System.out.println("Invalid selection. Please enter C to check in a book, or X to go back to the home screen.");
+                break;
+        }
+    }
+
+    public static void checkInBook()
+    {
+        System.out.print("Enter the ID of the book you want to check in: ");
+        int bookId = Integer.parseInt(userInput.nextLine());
+
+        if (bookId >= 1 && bookId <= bookCount && books[bookId - 1].isCheckedOut())
+        {
+            books[bookId - 1].checkIn();
+            System.out.println("The book was successfully checked in!");
+            System.out.println("Redirecting you back to the home page now...");
+        }
+        else
+        {
+            System.out.println("Invalid book ID or the book is not currently checked out.");
+            System.out.println("Redirecting you back to the home page now...");
+        }
+    }
+}
